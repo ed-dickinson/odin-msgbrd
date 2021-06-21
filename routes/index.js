ebb452b8f5d132cd16bc6426af10a0c8df628b26
ed-dickinson <edwardejd@gmail.com>
2021-06-21 12:37:26 +0100

    make it better

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Armando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+/* GET new post page */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Form'})
+});
+
+/*POST from form on new page*/
+router.post('/new', function(req, res, next){
+    messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+    
+    res.redirect('/');
+
+})
+
+module.exports = router;
